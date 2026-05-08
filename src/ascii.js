f452ede73137e775c5f0a945e676a94bde168a8b
webreflection <andrea.giammarchi@gmail.com>
2026-05-08 13:32:48 +0200

    recycle charCodeAt

--- src/ascii.js
+++ src/ascii.js
@@ -2,10 +2,16 @@ const { fromCharCode } = String;
 const toCharCode = Uint8Array.from.bind(Uint8Array);
 
 /**
+ * @param {string} c
+ * @returns
+ */
+const asCharCode = c => c.charCodeAt(0);
+
+/**
  * @param {string} str
  * @returns
  */
-export const encode = str => toCharCode(str, c => c.charCodeAt(0));
+export const encode = str => toCharCode(str, asCharCode);
 
 /**
  * @param {Uint8Array} view
