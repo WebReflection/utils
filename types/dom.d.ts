275105a024deb91a427d77e9d834bf73ee4647c2
webreflection <andrea.giammarchi@gmail.com>
2026-08-06 13:58:21 +0200

    better export for the fragment class

--- types/dom.d.ts
+++ types/dom.d.ts
@@ -1,6 +1,6 @@
 import * as content from './dom/content.js';
 import * as observer from './dom/observer.js';
 import * as signals from './dom/signals.js';
-import fragment from './dom/fragment.js';
+import Fragment from './dom/fragment.js';
 import diff from './dom/diff.js';
-export { content, diff, fragment, observer, signals };
+export { Fragment, content, diff, observer, signals };
