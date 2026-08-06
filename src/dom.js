beeeac89e48b12b632ddfe302280850ada470677
webreflection <andrea.giammarchi@gmail.com>
2026-08-06 15:16:24 +0200

    added utils/dom as generic DOM related export

--- src/dom.js
+++ src/dom.js
@@ -2,6 +2,8 @@ import * as content from './dom/content.js';
 import * as observer from './dom/observer.js';
 import * as signals from './dom/signals.js';
 import Fragment from './dom/fragment.js';
+import comment from './dom/comment.js';
 import diff from './dom/diff.js';
+import range from './dom/range.js';
 
-export { Fragment, content, diff, observer, signals };
+export { Fragment, comment, content, diff, observer, range, signals };
--- types/dom.d.ts
+++ types/dom.d.ts
@@ -2,5 +2,7 @@ import * as content from './dom/content.js';
 import * as observer from './dom/observer.js';
 import * as signals from './dom/signals.js';
 import Fragment from './dom/fragment.js';
+import comment from './dom/comment.js';
 import diff from './dom/diff.js';
-export { Fragment, content, diff, observer, signals };
+import range from './dom/range.js';
+export { Fragment, comment, content, diff, observer, range, signals };
