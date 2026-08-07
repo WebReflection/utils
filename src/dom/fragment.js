924413b3fa6a638bae14100b52305fb38be2eeb0
webreflection <andrea.giammarchi@gmail.com>
2026-08-07 10:31:25 +0200

    fixed dom-fragment constructor + added other methods

--- src/dom/fragment.js
+++ src/dom/fragment.js
@@ -23,41 +23,37 @@ export default class Fragment extends custom(DocumentFragment) {
    * @param {DocumentFragment} fragment
    */
   constructor(fragment) {
+    const { firstChild } = fragment;
     super(fragment);
-    this.#firstChild = fragment.insertBefore(createComment('<>'), fragment.firstChild);
+    this.#firstChild = fragment.insertBefore(createComment('<>'), firstChild);
     this.#lastChild = fragment.appendChild(createComment('</>'));
     this.#nodes = [...fragment.childNodes];
   }
 
-  get firstChild() {
-    return this.#firstChild;
-  }
+  get firstChild() { return this.#firstChild }
+  get lastChild() { return this.#lastChild }
 
-  get lastChild() {
-    return this.#lastChild;
-  }
+  get previousSibling() { return this.#firstChild.previousSibling }
+  get nextSibling() { return this.#lastChild.nextSibling }
 
-  get parentNode() {
-    return this.#firstChild.parentNode;
-  }
+  get parentNode() { return this.#firstChild.parentNode }
 
-  /**
-   * @param {...(Node | string)} nodes
-   */
-  before(...nodes) {
-    this.#firstChild.before(...nodes);
-  }
+  /** @param {...(Node | string)} nodes */
+  after(...nodes) { this.#lastChild.after(...nodes) }
 
-  remove() {
-    this.#drop().remove();
-  }
+  /** @param {...(Node | string)} nodes */
+  append(...nodes) { this.#lastChild.before(...nodes) }
 
-  /**
-   * @param {Node} node
-   */
-  replaceWith(node) {
-    this.#drop().replaceWith(node);
-  }
+  /** @param {...(Node | string)} nodes */
+  before(...nodes) { this.#firstChild.before(...nodes) }
+
+  /** @param {...(Node | string)} nodes */
+  prepend(...nodes) { this.#firstChild.after(...nodes) }
+
+  remove() { this.#drop().remove() }
+
+  /** @param {Node} node */
+  replaceWith(node) { this.#drop().replaceWith(node) }
 
   valueOf() {
     let firstChild = /** @type {ChildNode} */ (this.#firstChild), { parentNode } = firstChild;
--- types/dom/fragment.d.ts
+++ types/dom/fragment.d.ts
@@ -7,15 +7,19 @@ export default class Fragment extends Fragment_base {
     constructor(fragment: DocumentFragment);
     get firstChild(): Comment;
     get lastChild(): Comment;
+    get previousSibling(): ChildNode | null;
+    get nextSibling(): ChildNode | null;
     get parentNode(): ParentNode | null;
-    /**
-     * @param {...(Node | string)} nodes
-     */
+    /** @param {...(Node | string)} nodes */
+    after(...nodes: (Node | string)[]): void;
+    /** @param {...(Node | string)} nodes */
+    append(...nodes: (Node | string)[]): void;
+    /** @param {...(Node | string)} nodes */
     before(...nodes: (Node | string)[]): void;
+    /** @param {...(Node | string)} nodes */
+    prepend(...nodes: (Node | string)[]): void;
     remove(): void;
-    /**
-     * @param {Node} node
-     */
+    /** @param {Node} node */
     replaceWith(node: Node): void;
     valueOf(): this;
 }
